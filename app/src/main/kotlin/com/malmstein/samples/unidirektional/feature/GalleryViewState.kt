d6a3dea6971a54169186fe5893c7357a2876ef14
David González <malmstein@gmail.com>
2019-03-04 12:50:57 +0100

    better naming

--- app/src/main/kotlin/com/malmstein/samples/unidirektional/feature/GalleryViewState.kt
+++ app/src/main/kotlin/com/malmstein/samples/unidirektional/feature/GalleryViewState.kt
@@ -3,5 +3,5 @@ package com.malmstein.samples.unidirektional.feature
 import com.malmstein.samples.unidirektional.models.Failure
 import com.malmstein.samples.unidirektional.models.Success
 
-data class GalleryViewState(val photos: List<Photo> = emptyList()) : Success.ViewEvent()
+data class GalleryViewState(val photos: List<Photo> = emptyList()) : Success.ViewState()
 data class GalleryFailure(val e: Exception) : Failure.FeatureFailure()
